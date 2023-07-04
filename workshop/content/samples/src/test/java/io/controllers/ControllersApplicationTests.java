b65e4a7f11ae387de98815e791eaf732c8a289cf
tiffany jernigan <tfs1@vmware.com>
2023-07-04 16:58:49 -0700

    Add first exercise and spring initializr code

---
+++ workshop/content/samples/src/test/java/io/controllers/ControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
